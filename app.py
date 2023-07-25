4b796af56778c7962ca2f12d4d63707f470f33b3
nwidyant9 <nwidyanto9.nw9@gmail.com>
2023-07-25 11:37:27 +0700

    add dashboard page

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
 
+# Add logo
 my_logo = 'https://raw.githubusercontent.com/nwidyant9/Project00/main/Pictures/gdash.png'
 
 # Load Data
@@ -33,9 +34,34 @@ mme1_2023['Status'] = np.where(mme1_2023['BD_percent'] <= mme1_2023['Target_perc
 # Define the app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 
+# Make option for dropdown
 machine_options = [{'label': mesin, 'value': mesin} for mesin in mme1_2023['Mesin'].unique()]
 
-# Define the layouts
+# Define dashboard layout
+dashboard_layout = html.Div([
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '99%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    html.Div(id='output-data-upload'),
+])
+
+# Display the MME1 layout
 mme1_layout = linreg_layout = dbc.Container(
     [
         html.H1(children='Dashboard MME 1 2023', className='mt-3 mb-4'),
@@ -65,6 +91,7 @@ mme1_layout = linreg_layout = dbc.Container(
     ],
 )
 
+# Display the Linear Regression layout
 linreg_layout = dbc.Container(
     [
         dbc.Row(
@@ -81,13 +108,14 @@ linreg_layout = dbc.Container(
     className="mt-4"
 )
 
+# Define the app layout
 app.layout = dbc.Container(
     fluid=True,
     children=[
         dcc.Location(id='url', refresh=False),
         dbc.NavbarSimple(
             children=[
-                dbc.NavItem(dbc.NavLink("Dashboard", href="/", active="exact")),
+                dbc.NavItem(dbc.NavLink("Dashboard", href="/dashboard", active="exact")),
                 dbc.NavItem(dbc.NavLink("Linear Regression (BETA)", href="/linreg", active="exact")),
                 dbc.DropdownMenu(
                     [dbc.DropdownMenuItem(dbc.NavLink("MME1", href="/mme1-2023", active="exact", style={'color': 'blue'})),
@@ -114,6 +142,8 @@ app.layout = dbc.Container(
 def display_page(pathname):
     if pathname == '/':
         return html.H1(children='Dashboard', className='mt-3 mb-4')
+    elif pathname == '/dashboard':
+        return dashboard_layout
     elif pathname == '/linreg':
         return linreg_layout
     elif pathname == '/mme1-2023':
@@ -146,6 +176,55 @@ def update_graph(value):
 def update_plots(value):
     line_fig, bar_fig = update_graph(value)
     return line_fig, bar_fig
+
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        dash_table.DataTable(
+            df.to_dict('records'),
+            [{'name': i, 'id': i} for i in df.columns]
+        ),
+
+        html.Hr(),  # horizontal line
+
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
+
+@callback(Output('output-data-upload', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
